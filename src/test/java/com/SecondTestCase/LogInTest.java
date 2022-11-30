2db955f20e763af54f0db4fd5afd8c234b2a3a43
MOHIUDDIN1985 <khanmohiuddin512@gmail.com>
2022-11-30 12:19:47 -0500

    email change

--- src/test/java/com/SecondTestCase/LogInTest.java
+++ src/test/java/com/SecondTestCase/LogInTest.java
@@ -17,17 +17,24 @@ public class LogInTest {
 	    driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.navigate().to("https://demoqa.com/");
-		Thread.sleep(2000);
+		Thread.sleep(3000);
 		((JavascriptExecutor) driver).executeScript("scroll(0,300)");
 	}
 	
 	@Test
 	public void Login() throws InterruptedException{ {
 		driver.findElement(By.xpath("//h5[text()='Elements']")).click();
-		Thread.sleep(2000);
+		Thread.sleep(3000);
 		driver.findElement(By.className("text")).click();
 		Thread.sleep(5000);
 	}
+	}
+	@Test
+	public void email() throws InterruptedException {
+		driver.findElement(By.id("userName")).sendKeys("Mohi");
+		Thread.sleep(2000);
+		driver.findElement(By.id("userEmail")).sendKeys("Mohi@gmail.com");
+		Thread.sleep(5000);
 	}
 	@AfterClass
 	public void Close() {	
